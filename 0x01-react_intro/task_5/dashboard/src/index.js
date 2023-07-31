65c786fbaf9a8808919c03c8bd7b22376b7e9fe3
wantonraven <josephngalu96@gmail.com>
2023-07-31 22:46:17 +0300

    delete the dist folder

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,8 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./App.scss";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+import App from './App/App';
+// import reportWebVitals from './reportWebVitals';
+import Notifications from './Notifications/Notifications';
 
-const el = document.getElementById("app");
-
-ReactDOM.render(<App />, el);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <React.StrictMode>
+    <div className='root-notifications'>
+      <Notifications />
+    </div>
+    <App />
+  </React.StrictMode>
+);
