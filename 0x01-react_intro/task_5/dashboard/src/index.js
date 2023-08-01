ba38acc589b531e320af213affa19869afe31f4c
wantonraven <josephngalu96@gmail.com>
2023-08-01 14:49:00 +0300

    made test

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello world from index.js!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import App from './App/App';
+import MyNotifications from './Notifications/Notifications';
+
+const rootElement = document.getElementById('root');
+
+createRoot(rootElement).render(
+  <>
+    <MyNotifications />
+    <App />
+  </>
+);
