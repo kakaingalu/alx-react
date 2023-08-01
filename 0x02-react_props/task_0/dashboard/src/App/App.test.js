794ac74597f1f3113ee03a33f978bc6e7fad61d6
wantonraven <josephngalu96@gmail.com>
2023-08-01 16:20:02 +0300

    1. Write the tests for each component

--- 0x02-react_props/task_0/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import App from './App';
 
 describe('<App />', () => {
     it("renders without crashing", () => {
-        const wrapper = render(<App />);
+        const wrapper = shallow(<App />);
         expect(wrapper).not.toBeNull();
     });
+
+    it('renders the Notifications component', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find(Notifications).length).toBe(1);
+      });
+    
+      it('renders the Header component', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find(Header).length).toBe(1);
+      });
+    
+      it('renders the Login component', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find(Login).length).toBe(1);
+      });
+    
+      it('renders the Footer component', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find(Footer).length).toBe(1);
+      });
 });
