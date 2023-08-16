25f05199b9d93a7320fba0cf816f75d59652b37c
wantonraven <josephngalu96@gmail.com>
2023-08-16 15:08:49 +0300

    0. Converting into an Immutable object using fromJS

---
+++ 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { getFullYear, getFooterCopy } from '../utils/utils'
+import { StyleSheet, css } from 'aphrodite'
+import { AppContext } from '../App/AppContext'
+
+function Footer (props) {
+  return (
+    <AppContext.Consumer>
+      {(value) => (
+        <div className={css(styles.AppFooter)}>
+          <p> Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+          {value.user.isLoggedIn
+            ? (
+            <p className={css(styles.contactUsStyle)}>
+              <a href='#'>Contact Us</a>
+            </p>
+              )
+            : null}
+        </div>
+      )}
+    </AppContext.Consumer>
+  )
+}
+
+const styles = StyleSheet.create({
+  AppFooter: {
+    borderTop: '3px solid #e0354b',
+    width: '100%',
+    position: 'absolute',
+    bottom: '0',
+    marginBottom: '0',
+    fontStyle: 'italic',
+    textAlign: 'center',
+    fontFamily: 'Arial, sans-serif'
+  },
+  contactUsStyle: {
+    marginTop: '5px'
+  }
+})
+
+export default Footer
---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,18 @@
+const { fromJS } = require('immutable');
+
+function getImmutableObject(object) {
+  if (typeof object !== 'object' || object === null) {
+    throw new Error('input must be a valid object');
+  }
+  return fromJS(object);
+}
+
+const obj = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132,
+};
+
+const results = getImmutableObject(obj);
+console.log(results);
