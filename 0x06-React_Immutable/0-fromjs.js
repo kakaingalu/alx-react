f1b6f1c1111ef73ae6f55530bf9263a37d07f661
wantonraven <josephngalu96@gmail.com>
2023-08-16 15:22:56 +0300

    change eslint soln

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,6 +1,9 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable no-unused-vars */
+/* eslint-disable space-before-function-paren */
 const { Map, fromJS } = require('immutable');
 
-function getImmutableObject(object) {
+function getImmutableObject (object) {
   if (typeof object !== 'object' || object === null) {
     throw new Error('Input must be a valid object');
   }
@@ -11,7 +14,7 @@ const obj = {
   fear: true,
   smell: -1033575916.9145899,
   wall: false,
-  thing: -914767132,
+  thing: -914767132
 };
 
 const results = getImmutableObject(obj);
