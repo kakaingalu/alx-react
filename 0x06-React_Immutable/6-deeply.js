1f62ae7233d30625aaa4a9a577d77c6e5dedc4c0
wantonraven <josephngalu96@gmail.com>
2023-08-17 17:06:27 +0300

    remove unecessary function

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,17 +1,6 @@
-import { Map, List } from 'immutable';
+import { Map } from 'immutable';
 
-export function mergeDeep(map1, map2) {
-  return map1.mergeWith((prev, next) => {
-    if (Map.isMap(prev)) {
-      return mergeDeep(prev, next);
-    } if (List.isList(prev) && List.isList(next)) {
-      return prev.concat(next);
-    }
-    return next;
-  }, map2);
-}
-
-export function mergeDeeplyElements(page1, page2) {
-  const mergedMap = mergeDeep(Map(page1), Map(page2));
-  return List(mergedMap.values());
+export default function mergeDeeplyElements(page1, page2) {
+  const mergedMap = Map(page1).mergeDeep(Map(page2));
+  return mergedMap;
 }
