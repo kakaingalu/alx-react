0f8e8d2f33f8e80b940858a3a58e66332e3a223d
wantonraven <josephngalu96@gmail.com>
2023-08-17 16:41:53 +0300

    use  withMutation method

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -9,6 +9,8 @@ const map = Map({
   6: 'Lucas',
 });
 
-const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
+const map2 = map.withMutations((values) => {
+  values.set(2, 'Benjamin').set(4, 'Oliver');
+});
 
 export { map, map2 };
