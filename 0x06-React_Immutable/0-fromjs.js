eb8de2ffc314a9bd632664ae31a53de83b7d1ff8
wantonraven <josephngalu96@gmail.com>
2023-08-17 15:18:39 +0300

    change import

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,7 +1,7 @@
-const Immutable = require('immutable');
+import fromJS from 'immutable';
 
 function getImmutableObject(object) {
-  return Immutable.fromJS(object);
+  return fromJS(object);
 }
 
 export default getImmutableObject;
