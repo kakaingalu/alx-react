1e525e87eb6a26e2a79ffdbdb43475fc3caee02e
wantonraven <josephngalu96@gmail.com>
2023-08-17 15:15:08 +0300

    eport function

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -4,10 +4,4 @@ function getImmutableObject(object) {
   return Immutable.fromJS(object);
 }
 
-const obj = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132,
-};
-console.log(getImmutableObject(obj));
+export default getImmutableObject;
