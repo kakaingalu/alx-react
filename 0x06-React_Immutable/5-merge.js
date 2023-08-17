102f4a5032a73eef023b0d6f6fb2a5776c33a022
wantonraven <josephngalu96@gmail.com>
2023-08-17 16:55:36 +0300

    correct error in the function

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,10 +1,9 @@
 import { List, Map } from 'immutable';
 
 export function concatElements(page1, page2) {
-  return List(page1.concat(page2));
+  return List(page1).concat(List(page2));
 }
 
 export function mergeElements(page1, page2) {
-  const mergedMap = Map(page1).merge(page2);
-  return List(mergedMap.values());
+  return Map(page1).merge(Map(page2));
 }
